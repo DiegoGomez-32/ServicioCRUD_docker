b17fda1a8fbfb00a304fe66c99debe4a36e3d08a
DiegoGomez-32 <diegoagomez@unicauca.edu.co>
2024-10-30 15:37:08 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://bdclase2_user:gzazXEKGTme1qSDevFnmcvDznchvwayP@dpg-csfsnq88fa8c73a46k3g-a.oregon-postgres.render.com/bdclase2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
